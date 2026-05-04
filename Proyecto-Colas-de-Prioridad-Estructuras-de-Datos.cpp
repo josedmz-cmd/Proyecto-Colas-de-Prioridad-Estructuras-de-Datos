ceeb65448f2c95e1346fc6c4613c29fe59d99e0c
josedmz-cmd <josedaniel.morazunigagames@gmail.com>
2026-05-03 23:19:01 -0600

    Add files via upload

--- Proyecto-Colas-de-Prioridad-Estructuras-de-Datos.cpp
+++ Proyecto-Colas-de-Prioridad-Estructuras-de-Datos.cpp
@@ -2,9 +2,72 @@
 //
 
 #include <iostream>
+#include <string>
+#include "Heap_Priority_Queue.h"
 
-int main()
-{
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::getline;
+
+
+void estadoColas() {
+    ;
+    ;
+    ;
+}
+
+void MenuTiquete() { //Print del menu
+    cout << "1. Agregar Tiquete" << endl;
+    cout << "2. Regresar" << endl;
+}
+
+void MenuUsuario() { //Print del menu
+    cout << "1. Agregar" << endl;
+    cout << "2. Eliminar" << endl;
+    cout << "3. Regresar" << endl;
+}
+
+void MenuAreas() { //Print del menu
+    cout << "1. Agregar" << endl;
+    cout << "2. Modificar Ventanillas" << endl;
+    cout << "3. Eliminar" << endl;
+    cout << "4. Regresar" << endl;
+}
+
+void MenuServicio() { //Print del menu
+    cout << "1. Agregar" << endl;
+    cout << "2. Eliminar" << endl;
+    cout << "3. Reorganizar" << endl;
+    cout << "4. Regresar" << endl;
+}
+
+void MenuAdministracion() { //Print del menu
+    cout << "1. Tipo de usuario" << endl;
+    cout << "2. Areas" << endl;
+    cout << "3. Servicios" << endl;
+    cout << "4. Limpiar colas y estadisticas" << endl;
+    cout << "5. Regresar" << endl;
+}
+
+void Menu() { //Print del menu
+    cout << "1. Tiquete" << endl;
+    cout << "2. Atender Tiquete" << endl;
+    cout << "3. Administracion" << endl;
+    cout << "4. Estadisticas" << endl;
+    cout << "5. Salir" << endl;
+}
+
+string leer_String(const string& mensaje) { //Leer el imput del usuario
+    string valor;
+    cout << mensaje;
+    getline(cin, valor);
+    return valor;
+}
+
+int main() {
+    Heap_Priority_Queue<Patient> colaPacientes(5);
     std::cout << "Hello World!\n";
 }
 
