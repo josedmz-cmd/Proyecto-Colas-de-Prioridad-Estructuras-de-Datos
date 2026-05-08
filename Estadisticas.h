59751b0a4e8f36eb05e1b4546643e5b712716c9e
josedmz-cmd <josedaniel.morazunigagames@gmail.com>
2026-05-08 01:39:45 -0600

    Add files via upload

--- Estadisticas.h
+++ Estadisticas.h
@@ -81,16 +81,16 @@ public:
 		getValor(servicios, servicio);
 	}
 
-	void getTiqueteUsuario(const string& usuario) {
-		getValor(usuarios, usuario);
+	int getTiqueteUsuario(const string& usuario) {
+		return getValor(usuarios, usuario);
 	}
 
-	void getTiqueteArea(const string& area) {
-		getValor(areas, area);
+	int getTiqueteArea(const string& area) {
+		return getValor(areas, area);
 	}
 
-	void getTiqueteVentanilla(const string& ventanilla) {
-		getValor(ventanillas, ventanilla);
+	int getTiqueteVentanilla(const string& ventanilla) {
+		return getValor(ventanillas, ventanilla);
 	}
 
 	void clearStats() {
@@ -100,8 +100,75 @@ public:
 		ventanillas.clear();
 	}
 
-	void printStats() { //no definitivo
-		//Se va terminar en otra ocasión
+	void printStats(Area** areasArray, int numAreas, Servicios** serviciosArray, int numServicios, Usuario** usuariosArray, int numUsuarios) { //Recibir arreglos globales
+		cout << "Estadisticas:" << endl;
+		cout << "Tiempo promedio por Area:" << endl;
+		if (numAreas == 0) {
+			cout << "No hay areas registradas.";
+		}
+		else {
+			for (int i = 0; i < numAreas; i++) {
+				if (areasArray[i] != nullptr) {
+					//Hay que hacer un getter que devuelva el tiempo promedio de espera
+					//Se necesita getter para codigo y descripcion
+					//cout << " " << areasArray[i]->getCodigo() << " - " << areasArray[i]->getDescripcion() << endl;
+				}
+			}
+		}
+		cout << endl;
+		cout << "Cantidad de tiquetes dispensados por Area" << endl;
+		if (numAreas == 0) {
+			cout << "No hay areas registradas.";
+		} else {
+			for (int i = 0; i < numAreas; i++) {
+				if (areasArray[i] != nullptr) {
+					//int dispensados = getTiqueteArea(areasArray{i}->getCodigo());
+					//cout << " " << areasArray[i]->getCodigo() << " - " << areasArray[i]->getDescripcion() << ": " << dispensados << " tiquetes" << endl;
+				}
+			}
+		}
+		cout << endl;
+		cout << "Cantidad de tiquetes dispensados por Ventanilla" << endl;
+		if (numAreas == 0) {
+			cout << "No hay areas registradas.";
+		} else {
+			bool hayVentanillas = false;
+			for (int i = 0; i < numAreas; i++) {
+				if (areasArray[i] != nullptr) {
+					//cout << "Área " << areasArray[i]->getCodigo() << ":" << endl;
+					hayVentanillas = true;
+				}
+			}
+			if (!hayVentanillas) {
+				cout << "   No hay ventanillas registradas." << endl;
+			}
+		}
+		cout << endl;
+		cout << "Cantidad de tiquetes solicitados por Servicio" << endl;
+		if (numServicios == 0) {
+			cout << "No hay servicios registrados." << endl;
+		} else {
+			for (int i = 0; i < numServicios; i++) {
+				if (serviciosArray[i] != nullptr) {
+					//int solicitados = getTiqueteServicio(serviciosArray[i]->getDescripcion());
+					//cout << " " << serviciosArray[i]->getDescripcion() << ": " << solicitados << " tiquetes" << endl;
+				}
+			}
+		}
+		cout << endl;
+		cout << "Cantidad de tiquetes emitidos por Usuario" << endl;
+		if (numUsuarios == 0) {
+			cout << "No hay tipos de usuario registrados." << endl;
+		} else {
+			for (int i = 0; i < numUsuarios; i++) {
+				if (usuariosArray[i] != nullptr) {
+					int emitidos = getTiqueteUsuario(usuariosArray[i]->getDescripcion());
+					cout << "   " << usuariosArray[i]->getDescripcion()
+						<< ": " << emitidos << " tiquetes" << endl;
+				}
+			}
+		}
+		cout << endl;
 	}
 };
 
--- Servicios.h
+++ Servicios.h
@@ -1,5 +1,64 @@
 #pragma once
-class Servicios
-{
+
+#include <iostream>;
+#include <string>;
+
+using std::string;
+using std::ostream;
+
+class Servicios {
+private:
+	string descripcion;
+	int prioridad;
+	string codArea;
+
+public:
+	Servicios(string descripcion, int prioridad, string codArea) {
+		this->descripcion = descripcion;
+		this->prioridad = prioridad;
+		this->codArea = codArea;
+	}
+
+	~Servicios() {
+
+	}
+
+	string getDescripcion() const {
+		return descripcion;
+	}
+	
+	int getPrioridad() const {
+		return prioridad;
+	}
+
+	string getCodigo() const {
+		return codArea;
+	}
+
+	void setDescripcion(const string& desc) {
+		descripcion = desc;
+	}
+
+	void setPrioridad(int prio) {
+		prioridad = prio;
+	}
+
+	void setCodigo(const string& cod) {
+		codArea = cod;
+	}
+
+	bool operator==(const Servicios& otro) const {
+		return descripcion == otro.descripcion;
+	}
+
+	bool operator!=(const Servicios& otro) const {
+		return descripcion != otro.descripcion;
+	}
+
+	friend ostream& operator<<(ostream& os, const Servicios& s) {
+		os << s.descripcion << " (Prioridad: " << s.prioridad
+			<< ", Área: " << s.codArea << ")";
+		return os;
+	}
 };
 
